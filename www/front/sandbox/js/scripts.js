b19a994b8f776f6a539955eb197331f646457dbd
Marek Juras <marek.juras@lundegaard.eu>
2014-02-16 15:26:39 +0100

    js sync from ftp

--- www/front/sandbox/js/scripts.js
+++ www/front/sandbox/js/scripts.js
@@ -5,7 +5,21 @@ $(document).ready(function() {
 	});
 	Shadowbox.init();
 
-        $('a.non-clickable').click(function(e) {
-            e.preventDefault();
-        })
+	$('a.non-clickable').click(function(e) {
+		e.preventDefault();
+	})
+
+	var galleryContainer = $('.gallery-container');
+	if (galleryContainer) {
+		var paginatorButton = $('.paginator a');
+		paginatorButton.eq(0).addClass('current');
+		paginatorButton.click(function(e){
+			e.preventDefault();
+			var i = $(this).index();
+			paginatorButton.removeClass('current');
+			$(this).addClass('current');
+			galleryContainer.children('div').not('.paginator, .clearfix').hide().eq(i).show();
+		});
+	}
 });// END document ready
+

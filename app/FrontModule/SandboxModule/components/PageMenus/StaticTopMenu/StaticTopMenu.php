c2f3072f08c8ea9c6db0a9d0ebd74f5fb8367052
Marek Juras <marek.juras@lundegaard.eu>
2015-03-15 13:33:35 +0100

    project specific resources, components (menus, galleries, entities, templates) - continued

---
+++ app/FrontModule/SandboxModule/components/PageMenus/StaticTopMenu/StaticTopMenu.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FrontModule\SandboxModule\Components\PageMenus;
+
+use Bubo;
+
+class StaticTopMenu extends Bubo\Navigation\PageMenu
+{
+
+	public function __construct($parent, $name, $lang)
+	{
+		parent::__construct($parent, $name, $lang);
+		$this->setLabelName('Static menu')->disableCaching();
+	}
+
+	public function setUpRenderer($renderer)
+	{
+		return $renderer;
+	}
+
+	// return configured traverser
+	public function getTraverser()
+	{
+		$traverser = $this->createLabelTraverser()->skipFirst();
+		return $traverser;
+	}
+
+}

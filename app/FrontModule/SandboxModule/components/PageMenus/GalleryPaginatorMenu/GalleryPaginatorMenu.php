e2ede534b610717bb252b1023fb6fad11181b57a
Marek Juras <marek.juras@lundegaard.eu>
2015-04-12 21:52:22 +0200

    template of gallery detail

---
+++ app/FrontModule/SandboxModule/components/PageMenus/GalleryPaginatorMenu/GalleryPaginatorMenu.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace FrontModule\SandboxModule\Components\PageMenus;
+
+use Nette\Utils\Html,
+    Bubo;
+
+class GalleryPaginatorMenu extends Bubo\Navigation\PageMenu
+{
+
+    public function __construct($parent, $name, $lang) {
+        parent::__construct($parent, $name, $lang);
+        $this->setLabelName('Galerie')->disableCaching();
+    }
+
+    public function setUpRenderer($renderer) {
+        $renderer->onRenderMenuItem = callback($this, 'renderMenuItem');
+
+        $newWrappers = array(
+                        'topLevel'      =>  null,
+                        'topLevelItem'  =>  null,
+                        'innerLevel'      =>  null,
+                        'innerLevelItem'      =>  null
+        );
+
+        $renderer->setWrappers($newWrappers);
+        return $renderer;
+    }
+
+    // return configured traverser
+    public function getTraverser() {
+        /* @var $traverser \Bubo\Traversing\RenderingTraversers\LabelTraverser */
+        $traverser = $this->createLabelTraverser();
+        return $traverser
+                    ->setEntity('page')
+                    ->skipFirst();
+    }
+
+    public function renderMenuItem(
+        $page,
+        $acceptedStates,
+        $menuItemContainer,
+        $level,
+        $horizontalLevel,
+        $highlight,
+        $numOfDescendants
+    ) {
+
+        $template = $this->initTemplate(__DIR__ . '/templates/menuItem.latte');
+
+        $template->page = $page;
+        $template->level = $horizontalLevel;
+        $template->numOfItems = $numOfDescendants;
+
+//        if ($horizontalLevel == 3) {
+//            $menuItemContainer->class .= ' last';
+//        }
+
+	    if ($horizontalLevel == 1) {
+	        $menuItemContainer->add(Html::el()
+	                            ->setHtml($template->__toString())
+	                            );
+	    }
+
+        return $menuItemContainer;
+
+    }
+
+}
--- www/front/sandbox/js/scripts.js
+++ www/front/sandbox/js/scripts.js
@@ -1,10 +1,23 @@
 // document ready
 $(document).ready(function() {
-	$('.slide-me').cycle({
-		timeout: 3300
-	});
+    $('.slide-me').cycle({
+        timeout: 3300
+    });
 
-	$('.scroller ul').liScroll();
+    $('.scroller ul').liScroll();
 
-	Shadowbox.init();
+    var galleryContainer = $('.gallery-container');
+    if (galleryContainer.length) {
+        var paginatorButton = $('.paginator a');
+        paginatorButton.eq(0).addClass('current');
+        paginatorButton.click(function(e){
+            e.preventDefault();
+            var i = $(this).index();
+            paginatorButton.removeClass('current');
+            $(this).addClass('current');
+            galleryContainer.children('div').not('.paginator, .clearfix').hide().eq(i).show();
+        });
+    }
+
+    Shadowbox.init();
 });// END document ready
